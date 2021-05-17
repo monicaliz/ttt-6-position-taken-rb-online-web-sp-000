63f871724860ac5cf3c4f757b55199514c9b9b75
Monica Liz Rodriguez <monica.liz.rodriguez@gmail.com>
2021-05-17 23:52:36 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(board, index)
+if index == 0 && board = []
+  return false
+elsif index = nil
+  return false
+else index == "X" || index == "O"
+  true 
+end
+end
