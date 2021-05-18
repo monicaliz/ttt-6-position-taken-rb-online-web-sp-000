cf68a61cedc25bc32056fcaf67a9331ba51e5311
Monica Liz Rodriguez <monica.liz.rodriguez@gmail.com>
2021-05-18 00:23:34 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 require 'pry'
 def position_taken?(board, index)
-  binding.pry
+  # binding.pry
 if board[index] == " "
   return false
 elsif board[index] == ""
